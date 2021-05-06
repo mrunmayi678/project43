c1c26121854ce3f6a01a062d8926ba0450527954
mrunmayi678 <73644077+mrunmayi678@users.noreply.github.com>
2021-05-06 16:09:23 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("C U T N I F O N ", 520,140)
+fill("lightblue")
+text("Hint: An activity that is natural to or the purpose of a person or thing !!", 520,160)
+
+fill("white")
+textSize(15)
+text("A T A D A S B A ", 200,300)
+fill("lightblue")
+text("Hint: It is an organized collection of structured information, or data !!", 200,320)
+
+}
